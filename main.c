148f29faa1111198c712baa9c6f908347a6f4318
owlbeatsmusic <swedengt06@gmail.com>
2023-07-13 16:14:28 +0200

    working colors for each pixel

--- main.c
+++ main.c
@@ -1,16 +1,7 @@
 #include <stdio.h>
 #include "sim-game.h"
 
-Tile main_grid[GLOBAL_SCREEN_HEIGHT][GLOBAL_SCREEN_WIDTH];
-
 int main() {
-	
-	 for (int y = 0; y < GLOBAL_SCREEN_HEIGHT; y++) {
-		for (int x = 0; x < GLOBAL_SCREEN_WIDTH; x++) {
-			main_grid[y][x].symbol = '.';
-		}
-	}
-
 	renderer_initialize();
 	return 0;
 }
--- sim-game.h
+++ sim-game.h
@@ -1,16 +1,18 @@
 #ifndef SIM_GAME_H_
 #define SIM_GAME_H_
 
-#define GLOBAL_SCREEN_WIDTH  100
-#define GLOBAL_SCREEN_HEIGHT 50
+#define GLOBAL_SCREEN_WIDTH  80
+#define GLOBAL_SCREEN_HEIGHT 24
 
 int renderer_initialize();
 int renderer_render_screen();
 
 typedef struct {
 	char symbol;
-} Tile;
+	char fg_color[6];
+	char bg_color[6];
+} ScreenPixel;
 
-extern Tile main_grid[GLOBAL_SCREEN_HEIGHT][GLOBAL_SCREEN_WIDTH];
+extern ScreenPixel renderer_screengrid[GLOBAL_SCREEN_HEIGHT][GLOBAL_SCREEN_WIDTH];
 
 #endif
